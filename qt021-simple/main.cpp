b5631b328071049f9d7a37af5e2a664f7e2189e0
Nosikov <nosevg@yandex.ru>
2020-12-31 16:00:37 +0700

    data31122020++

--- qt021-simple/main.cpp
+++ qt021-simple/main.cpp
@@ -1,12 +1,30 @@
 
 #include <QApplication>
 #include <QMainWindow>
+#include <QLabel>
+#include <QPushButton>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QMainWindow w;
-    w.show();
+    QMainWindow * w = new QMainWindow;
+    w -> resize(700,700);
+    w -> setWindowTitle("Eto okno");
+
+
+    QLabel * label = new QLabel("Eto text label",w);
+    label->setGeometry(20,100,300,30);
+    label -> show();
+
+    QPushButton * button = new QPushButton ("press me", w);
+    button ->setGeometry(20,20,100,40);
+     w -> show();
+     button -> show();
+
+    QObject::connect(button,SIGNAL(clicked()),w,SLOT(close()));
+
+
     return a.exec();
 }
 
